a6bc92477fb09a0382e5292d27037f3652ad4852
vanetk <57934057+vanetk@users.noreply.github.com>
2022-09-29 00:27:38 -0300

    ahora el logo explota
    el grid cambia de color

---
+++ javascript.js
@@ -0,0 +1,12 @@
+
+
+//ESTA ANIMACIÓN SALE DE https://confettijs.org/
+// Pass in the id of an element
+let confetti = new Confetti('explota');
+
+// Edit given parameters
+confetti.setCount(75);
+confetti.setSize(1);
+confetti.setPower(25);
+confetti.setFade(false);
+confetti.destroyTarget(true);
